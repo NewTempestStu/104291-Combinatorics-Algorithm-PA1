8823ce41e5f52bb67b69e94aad8c1bea1971de74
Zixuan Guan <115603775+NewTempestStu@users.noreply.github.com>
2024-03-21 15:34:23 +0800

    update comment

--- Q1code/order.c
+++ Q1code/order.c
@@ -2,50 +2,76 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Function to compare two subsets for qsort
+/**
+ * Compare function for qsort to sort subsets by size and then lexicographically
+ * @param a Pointer to a pointer to an integer array
+ * @param b Pointer to a pointer to an integer array
+ * @return -1 if a < b, 0 if a == b, 1 if a > b
+*/
 int compare(const void *a, const void *b) {
-    const int *ia = *(const int **)a;
-    const int *ib = *(const int **)b;
-    int sizeA = ia[0];
-    int sizeB = ib[0];
-    if (sizeA != sizeB) {
-        return sizeA - sizeB;
-    } else {
-        for (int i = 1; i <= sizeA; ++i) {
-            if (ia[i] != ib[i]) {
-                return ia[i] - ib[i];
+    const int *ia = *(const int **)a;       // cast to pointer to int
+    const int *ib = *(const int **)b;       // cast to pointer to int
+    int sizeA = ia[0];                      // First element stores the size of the subset
+    int sizeB = ib[0];                      // First element stores the size of the subset
+    if (sizeA != sizeB) {                   // First compare by size
+        return sizeA - sizeB;               // Smaller size comes first
+    } else {                                // If sizes are equal
+        for (int i = 1; i <= sizeA; ++i) {  // Then compare lexicographically
+            if (ia[i] != ib[i]) {           // First difference determines order
+                return ia[i] - ib[i];       // Smaller value comes first
             }
         }
     }
-    return 0;
+    return 0;                               // Both subsets are equal
 }
+/**
+ * Function to generate all subsets of a set of n elements
+ * @param n Number of elements in the set
+ * @param subsets Pointer to a pointer to an integer array to store the subsets
+ * @param pow_set_size Pointer to an unsigned int to store the size of the power set
+ * @return void
+ * @note The caller is responsible for freeing the memory allocated for the subsets
+ *     using the freeSubsets function
+ * @note The subsets are sorted by size and then lexicographically
+ * @note The first element of each subset stores the size of the subset
+*/
+void generateSubsets(int n, int ***subsets, unsigned int *pow_set_size) {   // Pointer to a pointer to an integer array
+    *pow_set_size = pow(2, n);                                              // Size of the power set is 2^n
+    *subsets = (int **)malloc(*pow_set_size * sizeof(int *));               // Allocate memory for the power set
+    int index = 0;                                                          // Index to store subsets
 
-void generateSubsets(int n, int ***subsets, unsigned int *pow_set_size) {
-    *pow_set_size = pow(2, n);
-    *subsets = (int **)malloc(*pow_set_size * sizeof(int *));
-    int index = 0;
-
-    for (unsigned int i = 0; i < *pow_set_size; i++) {
-        (*subsets)[index] = (int *)malloc((n + 1) * sizeof(int));
-        (*subsets)[index][0] = 0; // First element stores the size of the subset
-        for (int j = 0; j < n; j++) {
-            if (i & (1 << j)) {
-                (*subsets)[index][++(*subsets)[index][0]] = j + 1;
+    for (unsigned int i = 0; i < *pow_set_size; i++) {                      // Generate all 2^n subsets
+        (*subsets)[index] = (int *)malloc((n + 1) * sizeof(int));           // Allocate memory for each subset
+        (*subsets)[index][0] = 0;                                           // First element stores the size of the subset
+        for (int j = 0; j < n; j++) {                                       // Check each bit of the number
+            if (i & (1 << j)) {                                             // If jth bit is set
+                (*subsets)[index][++(*subsets)[index][0]] = j + 1;          // Add j+1 to the subset
             }
         }
-        index++;
+        index++;                                                            // Move to the next subset
     }
 
-    qsort(*subsets, *pow_set_size, sizeof(int *), compare);
+    qsort(*subsets, *pow_set_size, sizeof(int *), compare);                 // Sort the subsets
 }
 
-void freeSubsets(int **subsets, unsigned int pow_set_size) {
-    for (unsigned int i = 0; i < pow_set_size; i++) {
-        free(subsets[i]);
-    }
-    free(subsets);
+/**
+ * Function to free the memory allocated for the subsets
+ * @param subsets Pointer to a pointer to an integer array
+ * @param pow_set_size Size of the power set
+ * @return void
+*/
+void freeSubsets(int **subsets, unsigned int pow_set_size) {                // Pointer to a pointer to an integer array
+    for (unsigned int i = 0; i < pow_set_size; i++) {                       // Free memory for each subset
+        free(subsets[i]);                                                   // Free memory for each subset
+    }       
+    free(subsets);                                                          // Free memory for the power set
 }
 
+/**
+ * Function to print all subsets of a set of n elements
+ * @param n Number of elements in the set
+ * @return void
+*/
 void printSubsets(int n) {
     unsigned int pow_set_size;
     int **subsets;
@@ -138,6 +164,7 @@ void printSuccessor(int n, int *subset, int subsetSize) {
         }
     }
     
+    // Print successor
     if (rank != -1 && rank + 1 < pow_set_size) {
         printf("Successor of the given subset is {");
         for (int j = 1; j <= subsets[rank + 1][0]; j++) {
